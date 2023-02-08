a20d235abca974f584d54ca393be7d2c865cf912
ShiderS <irbis26813@gmail.com>
2023-02-08 16:47:38 +0500

    fix and add screenshots of the bot

--- main.py
+++ main.py
@@ -143,10 +143,8 @@ def edit_list_valutes_user(message, new_list_valutes):
 @dp.message_handler(text=[''])
 async def echo_handler(message: types.Message):
     try:
-        # Send copy of the received message
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        # But not all the types is supported to be copied so need to handle it
         await message.answer('Nice try!')
 
 
